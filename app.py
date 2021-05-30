8be909099c6b2367e581765e17638c5114c5f9d1
hardikroutray <hardik.routray@rutgers.edu>
2021-05-30 03:00:33 -0400

    app update

--- app.py
+++ app.py
@@ -68,9 +68,22 @@ st.markdown("""
 """, unsafe_allow_html=True)
 
 #model = load_model("./bestCNN2D.h5")
-os.system("wget https://github.com/hardikroutray/testapp/blob/master/bestCNN2D.h5")
-model = load_model("./bestCNN2D.h5") 
+import zipfile
+import tempfile
 
+stream = st.file_uploader('TF.Keras model file (.h5py.zip)', type='zip')
+if stream is not None:
+  myzipfile = zipfile.ZipFile(stream)
+  with tempfile.TemporaryDirectory() as tmp_dir:
+    myzipfile.extractall(tmp_dir)
+    root_folder = myzipfile.namelist()[0] # e.g. "model.h5py"
+    model_dir = os.path.join(tmp_dir, root_folder)
+    #st.info(f'trying to load model from tmp dir {model_dir}...')
+    model = tf.keras.models.load_model(model_dir)
+
+
+#os.system("wget https://github.com/hardikroutray/testapp/blob/master/bestCNN2D.h5")
+#model = load_model("./bestCNN2D.h5") 
 
 def model_prediction(img):
   #img = image.load_img(img_path, grayscale=True, target_size=(150,150)) #target_size=(28,28), grayscale=True
